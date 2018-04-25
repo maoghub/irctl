43d649405f058e6a6ef923b82ce8a34fe459489f
maoghub <maoghub@gmail.com>
2018-04-25 15:23:49 -0700

    Clean up update_binary

--- init/update_binary.sh
+++ init/update_binary.sh
@@ -2,17 +2,6 @@
 
 set -e 
 
-export GOROOT=/usr/local/go
-export GOPATH=~/go
-
 cd ${GOPATH}/src/irctl/server
-#git rebase origin/master 
-
-#cd server
-#go get ./...
-
-#ln -fs ${GOROOT}/src/github.com/irctl/server/control ~/go/src/irctl/server/control
-
 go build server.go
-
 sudo cp server /usr/local/bin/irrigation_control
